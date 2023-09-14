32bfcf0eb7291b217b8fcc028b5a36c3d30ef8e1
Jacob Lang <jacoblang72@comcast.net>
2023-09-14 16:39:07 -0400

    moreTodos

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react"
-import { getTodos } from "./actions/preferenceActions";
+import { getTodos, postTodo } from "./actions/preferenceActions";
 
 export default function TodoForm() {
     const [todos,setTodos] = useState([]);
+    const [todoInQue,setTodoInQue] = useState(""); 
+
+    const change = e => {
+        e.preventDefault(); 
+        setTodoInQue(e.target.value)
+    }
 
     useEffect(()=> {
         fetchData(); 
@@ -10,8 +16,13 @@ export default function TodoForm() {
 
     const fetchData = () => {
         getTodos().then(res=> {
-            console.log(res.data);
             setTodos(res.data);  
+        })
+    }
+    const addData = () => {
+        postTodo(todoInQue).then(()=> {
+            fetchData();
+            setTodoInQue(""); 
         })
     }
 
@@ -20,8 +31,8 @@ export default function TodoForm() {
             <div id="container">
                 <h2>To-do List</h2>
                 <div id = "smaller">
-                <input id="add" type="text" />
-                <span className="material-symbols-outlined">
+                <input value = {todoInQue} onChange={change} id="add" type="text" />
+                <span onClick = {addData}className="material-symbols-outlined">
                     add_circle
                 </span>
                 </div>
--- src/actions/preferenceActions.js
+++ src/actions/preferenceActions.js
@@ -6,3 +6,7 @@ export const getTodos = () => {
     return axios.get(baseUrl)
     .catch(err=> console.error(err.message)); 
 }
+export const postTodo = (todo) => {
+    const obj = {id : Date.now(), completed : false, description : todo }
+    return axios.post(baseUrl,obj)
+}
