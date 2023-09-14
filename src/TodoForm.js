a23dc109d6b6a32c8e6aa17fc60520d551239302
Jacob Lang <jacoblang72@comcast.net>
2023-09-14 16:12:22 -0400

    wip

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,7 +2,8 @@ export default function TodoForm() {
     return (
         <div id="todoContainer">
             <div id="container">
-                <h2>Todo List</h2>
+                <h2>To-do List</h2>
+                <input  id = "add" type = "text" />
             </div>
         </div>
     )
