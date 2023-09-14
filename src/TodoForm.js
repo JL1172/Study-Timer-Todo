3e935a3cc0609b2fcd57ee72eff6387830419800
Jacob Lang <jacoblang72@comcast.net>
2023-09-14 16:48:34 -0400

    wip

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,46 +1,60 @@
 import { useEffect, useState } from "react"
-import { getTodos, postTodo } from "./actions/preferenceActions";
+import { getTodos, postTodo, putTodo } from "./actions/preferenceActions";
 
 export default function TodoForm() {
-    const [todos,setTodos] = useState([]);
-    const [todoInQue,setTodoInQue] = useState(""); 
+    const [todos, setTodos] = useState([]);
+    const [todoInQue, setTodoInQue] = useState("");
 
     const change = e => {
-        e.preventDefault(); 
+        e.preventDefault();
         setTodoInQue(e.target.value)
     }
 
-    useEffect(()=> {
-        fetchData(); 
-    },[])
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     const fetchData = () => {
-        getTodos().then(res=> {
-            setTodos(res.data);  
+        getTodos().then(res => {
+            setTodos(res.data);
         })
     }
     const addData = () => {
-        postTodo(todoInQue).then(()=> {
+        postTodo(todoInQue).then(() => {
             fetchData();
-            setTodoInQue(""); 
+            setTodoInQue("");
         })
     }
-
+    const completeData = (todo) => {
+        putTodo(todo).then(() => {
+            fetchData();
+        })
+    }
+    const undoCompletion = todo => {
+        putTodo(todo).then(() => {
+            fetchData();
+        })
+    }
     return (
         <div id="todoContainer">
             <div id="container">
                 <h2>To-do List</h2>
-                <div id = "smaller">
-                <input value = {todoInQue} onChange={change} id="add" type="text" />
-                <span onClick = {addData}className="material-symbols-outlined">
-                    add_circle
-                </span>
+                <div id="smaller">
+                    <input value={todoInQue} onChange={change} id="add" type="text" />
+                    <span onClick={addData} className="material-symbols-outlined">
+                        add_circle
+                    </span>
                 </div>
-                {todos.map(n=> {
-                    return <div className = "list" key = {n.id}>
-                        <span className = "description">{n.description}</span>
-                        <button className="completed">{n.completed === true ? "delete" : "complete"}</button >
-                        </div>
+                {todos.map(n => {
+                    return <div className="list" key={n.id}>
+                        <span className="description">{n.description}</span>
+                        <button onClick={() => completeData(n)} className="completed">{n.completed === true ? "delete" : "complete"}
+                        </button >
+                        {n.completed ? 
+                        <span style = {{cursor : "pointer"}} onClick = {()=>undoCompletion(n)} className="material-symbols-outlined">
+                            undo
+                        </span> : ""}
+                    </div>
                 })}
             </div>
         </div>
--- src/actions/preferenceActions.js
+++ src/actions/preferenceActions.js
@@ -10,3 +10,11 @@ export const postTodo = (todo) => {
     const obj = {id : Date.now(), completed : false, description : todo }
     return axios.post(baseUrl,obj)
 }
+export const putTodo = (todo) => {
+    const modded = {...todo, completed : true}
+    return axios.put(`${baseUrl}/${todo.id}`,modded)
+}
+export const secondPutTodo =(todo) => {
+    const modded = {...todo, completed : false}
+    return axios.put(`${baseUrl}/${todo.id}`,modded)
+}
